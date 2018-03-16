bed118857d461394ba8cff0f2ac50f8e91a009de
Tarah Nixon <tarah.sage.nixon@gmail.com>
2018-03-16 20:26:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ function subtract(a, b) {
   return a - b;
 }
 
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(a) {
+  return a++;
+}
+
+function dec(a) {
+  return a--;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
